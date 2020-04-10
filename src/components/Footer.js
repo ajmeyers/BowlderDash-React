dc19f1dd0032d882da1330d0a1280f6d4afc471d
Andrew Meyers <andrew.meyers.j@gmail.com>
2020-04-10 17:54:12 -0400

    Added Header and Footer skeleton components

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <center>
+                    <h1>Contact Information</h1>
+                </center>
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <center>
+                <h1>Welcome To BowlderDash!</h1>
+                <p>Entertainment for all ages.</p>
+                </center>
+            </div>
+        );
+    }
+}
+
+export default Header;
