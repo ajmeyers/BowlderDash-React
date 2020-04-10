6f416cf02f627fd789a191cef875e4c823cf7015
Andrew Meyers <andrew.meyers.j@gmail.com>
2020-04-10 18:44:12 -0400

    Updated Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { Jumbotron } from 'reactstrap';
+import logo from '../img/logo.png';
 
 
 class Header extends Component {
     render() {
         return (
-            <div className="Header">
-                <center>
-                <h1>Welcome To BowlderDash!</h1>
-                <p>Entertainment for all ages.</p>
-                </center>
-            </div>
+            <React.Fragment>
+                <Jumbotron fluid>
+                    <div className="headerJumbotron">
+                        <div className="container">
+                            <div className="row">
+                                <div class="col-4 col-sm-3 col-md-2 align-self-center">
+                                        <img src={logo} alt="Logo" width="250px" height="250px"/>
+                                </div>
+                                <div className="col-4 col-sm-3 col-md-2 align-self-center">
+                                </div>
+                                <div className="col-6">
+                                    <h1 className="display-4"><b>BowlderDash Lanes</b></h1>
+                                    <p><b>Entertainment for all ages!</b></p>
+                                </div>
+                                <div className="col align-self-right">
+                                    <p><b>Created by Andrew Meyers</b></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+            </React.Fragment>
         );
     }
 }
