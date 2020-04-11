f2c44dc91c651507aab224de7e849d9d9857c291
Andrew Meyers <andrew.meyers.j@gmail.com>
2020-04-10 21:57:20 -0400

    Code Formatting

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,13 +4,13 @@ import { Carousel } from 'react-responsive-carousel';
 import bowlingcarousel1 from '../img/bowling_carousel1.jpg';
 import bowlingcarousel2 from '../img/bowling_carousel2.jpg';
 import bowlingcarousel3 from '../img/bowling_carousel3.jpg';
- 
+
 class CarouselBowl extends Component {
     render() {
         return (
             <Carousel width="700px" useKeyboardArrows autoPlay infiniteLoop>
                 <div>
-                    <img src={bowlingcarousel1} alt="bc1"/>
+                    <img src={bowlingcarousel1} alt="bc1" />
                     <p className="legend">Bowling!</p>
                 </div>
                 <div>
