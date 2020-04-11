325fec853986fa5aa2d148e1a6179ef16e544152
Andrew Meyers <andrew.meyers.j@gmail.com>
2020-04-10 20:55:49 -0400

    Started on Content Component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import bowlingHome from '../img/bowling_home.png';
+
 
 class Content extends Component {
     render() {
         return (
-            <div className="Content">
-                <center>
-                <h1>Main Content</h1>
-                </center>
-            </div>
+            <React.Fragment>
+                <div className="container">
+                    <div className="row row-content align-items-center">
+                        <div className="col">
+                            <div className="media">
+                                <img className="d-flex mr-3" src={bowlingHome} height="150" width="150" alt="bowling home"/>
+                                <div className="media-body align-self-center">
+                                    <h3>Welcome to BowlderDash Lanes!</h3>
+                                    <p className="d-none d-sm-block">
+                                        The premier Entertainment Center for Bowling, Arcade Games, and Parties! Come visit us in
+                                        Atlanta.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
