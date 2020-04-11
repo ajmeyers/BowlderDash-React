c4fed32350222176e5ca1f0802f221d09befc8aa
Andrew Meyers <andrew.meyers.j@gmail.com>
2020-04-10 21:19:51 -0400

    Tweaking Carousel

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Carousel from './Carousel';
 import bowlingHome from '../img/bowling_home.png';
 
 
@@ -6,20 +7,26 @@ class Content extends Component {
     render() {
         return (
             <React.Fragment>
+                <br />
                 <div className="container">
                     <div className="row row-content align-items-center">
                         <div className="col">
                             <div className="media">
-                                <img className="d-flex mr-3" src={bowlingHome} height="150" width="150" alt="bowling home"/>
+                                <img className="d-flex mr-3" src={bowlingHome} height="150" width="150" alt="bowling home" />
                                 <div className="media-body align-self-center">
                                     <h3>Welcome to BowlderDash Lanes!</h3>
                                     <p className="d-none d-sm-block">
                                         The premier Entertainment Center for Bowling, Arcade Games, and Parties! Come visit us in
                                         Atlanta.
                                     </p>
+                                    <br />
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div>
+                        <center><Carousel /></center>
+                        <br />
                     </div>
                 </div>
             </React.Fragment>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,32 +4,32 @@ class Footer extends Component {
     render() {
         return (
             <React.Fragment>
-                    <div className="footer container">
-                        <div className="row">
-                            <div className="col-4 col-sm-2 offset-1">
-                                <h5>Links</h5>
-                                <ul className="list-unstyled">
-                                    <li><a href="https://github.com/ajmeyers">Home</a></li>
-                                    <li><a href="https://github.com/ajmeyers">Blog</a></li>
-                                    <li><a href="https://github.com/ajmeyers">Contact</a></li>
-                                </ul>
-                            </div>
-                            <div className="col-6 col-sm-5 text-center">
-                                <h5>Social Media</h5>
+                <div className="footer container">
+                    <div className="row">
+                        <div className="col-4 col-sm-2 offset-1">
+                            <h5>Links</h5>
+                            <ul className="list-unstyled">
+                                <li><a href="https://github.com/ajmeyers">Home</a></li>
+                                <li><a href="https://github.com/ajmeyers">Blog</a></li>
+                                <li><a href="https://github.com/ajmeyers">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div className="col-6 col-sm-5 text-center">
+                            <h5>Social Media</h5>
 
-                                <a href="https://www.facebook.com/">Facebook</a><br />
-                                <a href="https://www.instagram.com/">Instagram</a><br />
-                                <a href="https://www.twitter.com/">Twitter</a><br />
-                                <a href="https://www.youtube.com/">Youtube</a><br />
-                            </div>
-                            <div className="col-sm-2 text-center">
-                                <a role="button" className="btn btn-link" href="tel:+1-555-555-5555"><i className="fa fa-phone"></i>
+                            <a href="https://www.facebook.com/">Facebook</a><br />
+                            <a href="https://www.instagram.com/">Instagram</a><br />
+                            <a href="https://www.twitter.com/">Twitter</a><br />
+                            <a href="https://www.youtube.com/">Youtube</a><br />
+                        </div>
+                        <div className="col-sm-2 text-center">
+                            <a role="button" className="btn btn-link" href="tel:+1-555-555-5555"><i className="fa fa-phone"></i>
                         1-555-555-5555</a><br />
-                                <a role="button" className="btn btn-link" href="mailto:contact@bowlderdash.com"><i
-                                    className="fa fa-envelope-o"></i>contact@bowlderdash.com</a>
-                            </div>
+                            <a role="button" className="btn btn-link" href="mailto:contact@bowlderdash.com"><i
+                                className="fa fa-envelope-o"></i>contact@bowlderdash.com</a>
                         </div>
                     </div>
+                </div>
             </React.Fragment>
         );
     }
